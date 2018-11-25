770bba0f4fe9ebceb16e3ec9de0035dfdfa41c8e
jiazhongfei <191495349@qq.com>
2018-11-25 20:09:37 +0800

    create-project

---
+++ src/main/java/com/example/demojekins/DemoJekinsApplication.java
@@ -0,0 +1,12 @@
+package com.example.demojekins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJekinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJekinsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/demojekins/DemoJekinsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demojekins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoJekinsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
